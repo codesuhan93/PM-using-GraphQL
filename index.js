3555551b0c3c5d3d79efed1a261788157c159b02
codesuhan93 <moonhiraj@gmail.com>
2022-11-16 15:38:16 +0500

    deployment

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
 const connectDB = require("./config/db");
+const path = require("path");
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -21,5 +22,11 @@ app.use(
     graphiql: process.env.NODE_ENV === "development",
   })
 );
+
+app.use(express.static("public"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+});
 
 app.listen(port, console.log(`Server running on port ${port}`));
